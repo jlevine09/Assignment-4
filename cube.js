1357856c7063df27f6463184e07045a9e3df9f9d
Jlevine <fundragonstudios@gmail.com>
2018-04-21 21:08:54 -0700

    changed to viewing and camera

--- cube.js
+++ cube.js
@@ -204,7 +204,7 @@ var gl;
  	var viewMatrix = new Float32Array(16);
  	var projMatrix = new Float32Array(16);
  	mat4.identity(worldMatrix);
- 	mat4.lookAt(viewMatrix, [0, 0, -8], [0, 0, 0], [0, 1, 0]);
+ 	mat4.lookAt(viewMatrix, [0, 0, -30], [0, 0, 0], [0, 1, 0]);
  	mat4.perspective(projMatrix, glMatrix.toRadian(45), canvas.width / canvas.height, 0.1, 1000.0);
  
  	gl.uniformMatrix4fv(matWorldUniformLocation, gl.FALSE, worldMatrix);
